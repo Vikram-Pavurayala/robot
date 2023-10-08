9d2ac37eb4e7b58dc41f74213debddc94955f105
Vikram-Pavurayala <127219131+Vikram-Pavurayala@users.noreply.github.com>
2023-10-08 11:39:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+ setTimeout(() => {
+            document.querySelector('.preloader').style.display = 'none';
+        }, 5000); // You can adjust the delay as needed
+
+        function toggleMobileMenu() {
+            const mobileMenu = document.querySelector('.mobile-menu');
+            mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
+        }
+        
